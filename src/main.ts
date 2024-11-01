79a9a012bb62e6e6a831433503309c788244561f
JasmineYil <71649452+JasmineYil@users.noreply.github.com>
2024-11-01 21:25:40 +0100

    added .ts file extension to imports in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { toggleComments } from './modules/showHideComments';
-import { addComment } from './modules/addComment';
-import { getBearData } from './modules/fetchBearData';
+// Importing TypeScript files directly - Necessary for module resolution during bundling
+// @ts-expect-error: Module resolution for TypeScript files (.ts)
+import { toggleComments } from './modules/showHideComments.ts'; // @ts-expect-error: Module resolution for TypeScript files (.ts)
+import { addComment } from './modules/addComment.ts'; // @ts-expect-error: Module resolution for TypeScript files (.ts)
+import { getBearData } from './modules/fetchBearData.ts';
 
 window.onload = () => {
   console.log('Initializing Application ...');
