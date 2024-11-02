389419a51fe5956fa0e52a9a4b69b2bb7f193963
JasmineYil <71649452+JasmineYil@users.noreply.github.com>
2024-11-02 20:41:05 +0100

    Updated files for Accessibility Playground

--- src/modules/showHideComments.ts
+++ src/modules/showHideComments.ts
@@ -1,42 +1,18 @@
-// export const toggleComments = (): void => {
-//   const showHideBtn = document.querySelector<HTMLElement>('.show-hide');
-//   const commentWrapper =
-//     document.querySelector<HTMLElement>('.comment-wrapper');
-//
-//   if (showHideBtn !== null && commentWrapper !== null) {
-//     commentWrapper.style.display = 'none';
-//
-//     showHideBtn.addEventListener('click', () => {
-//       const showHideText = showHideBtn.textContent;
-//       if (showHideText === 'Show comments') {
-//         showHideBtn.textContent = 'Hide comments';
-//         commentWrapper.style.display = 'block';
-//       } else {
-//         showHideBtn.textContent = 'Show comments';
-//         commentWrapper.style.display = 'none';
-//       }
-//     });
-//   } else {
-//     console.error('Required elements not found in the DOM');
-//   }
-// };
-
 export const toggleComments = (): void => {
   const showHideBtn = document.querySelector<HTMLElement>('.show-hide');
-  const commentWrapper =
-    document.querySelector<HTMLElement>('.comment-wrapper');
+  const commentWrapper = document.querySelector<HTMLElement>('.comment-wrapper');
 
   if (showHideBtn !== null && commentWrapper !== null) {
-    commentWrapper.setAttribute('hidden', '');
+    commentWrapper.style.display = 'none';
 
     showHideBtn.addEventListener('click', () => {
-      const isHidden = commentWrapper.hasAttribute('hidden');
+      const isHidden = commentWrapper.style.display === 'none';
       if (isHidden) {
-        commentWrapper.removeAttribute('hidden');
+        commentWrapper.style.display = 'block';
         showHideBtn.textContent = 'Hide comments';
         showHideBtn.setAttribute('aria-expanded', 'true');
       } else {
-        commentWrapper.setAttribute('hidden', '');
+        commentWrapper.style.display = 'none';
         showHideBtn.textContent = 'Show comments';
         showHideBtn.setAttribute('aria-expanded', 'false');
       }
